d7b6414ec37cf648e7ce760eb40371d8e838473f
lendale <nasibag1>
2016-08-29 04:38:17 +0800

    *Added printHashKey() method

--- app/src/main/java/com/acg/midtermrequirement/LoginActivity.java
+++ app/src/main/java/com/acg/midtermrequirement/LoginActivity.java
@@ -1,13 +1,42 @@
 package com.acg.midtermrequirement;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private String TAG = LoginActivity.this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        printHashkey();
         setContentView(R.layout.activity_login);
+    }
+
+    private void printHashkey(){
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.acg.midtermrequirement",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d(TAG, "Hash Key: " + Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
     }
 }
