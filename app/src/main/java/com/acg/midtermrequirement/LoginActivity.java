432a8bdf058fd81fbb003e822585b194854b91fd
lendale <nasibag1>
2016-08-30 16:27:41 +0800

    Set OnClickListners for the Sign In options

--- app/src/main/java/com/acg/midtermrequirement/LoginActivity.java
+++ app/src/main/java/com/acg/midtermrequirement/LoginActivity.java
@@ -7,6 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -15,11 +25,55 @@ public class LoginActivity extends AppCompatActivity {
 
     private String TAG = LoginActivity.this.getClass().getSimpleName();
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private CallbackManager mCallbackManager;
+
+    private EditText etEmail;
+    private EditText etPass;
+    private TextView tvSignIn1;
+    private ImageView imgSignInFb;
+
+    private LinearLayout linSignInFb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        printHashkey();
+        findViews();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_login);
+    }
+
+    private void findViews() {
+        etEmail = (EditText) findViewById(R.id.et_email);
+        etPass = (EditText) findViewById(R.id.et_pass);
+        tvSignIn1 = (TextView) findViewById(R.id.tv_sign_in_1);
+        imgSignInFb = (ImageView) findViewById(R.id.img_sign_in_fb_2);
+        linSignInFb = (LinearLayout) findViewById(R.id.lin_sign_in_fb_1);
+    }
+
+    private void initOnclickListeners() {
+        tvSignIn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        imgSignInFb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        linSignInFb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     private void printHashkey(){
